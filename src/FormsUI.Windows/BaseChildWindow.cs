5ad2326b29e0ae120ede573fe021787863ab092d
daxnet <daxnet@outlook.com>
2019-07-10 21:52:51 +0800

    Adding the FormUI.Windows class library for dockable forms implementation.

---
+++ src/FormsUI.Windows/BaseChildWindow.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace FormsUI.Windows
+{
+    public partial class BaseChildWindow : DockContent
+    {
+        protected BaseChildWindow(bool hideOnClose = true)
+        {
+            HideOnClose = hideOnClose;
+        }
+
+        private BaseChildWindow()
+        {
+            InitializeComponent();
+        }
+
+        public event EventHandler DockWindowHidden;
+
+        public event EventHandler DockWindowShown;
+
+        public override string ToString() => Text;
+
+        protected override void OnDockStateChanged(EventArgs e)
+        {
+            
+        }
+
+        protected virtual void OnDockWindowHidden(EventArgs e)
+        {
+            DockWindowHidden?.Invoke(this, e);
+        }
+
+        protected virtual void OnDockWindowShown(EventArgs e)
+        {
+            DockWindowShown?.Invoke(this, e);
+        }
+    }
+}
---
+++ src/FormsUI.Windows/ToolStripMerge.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsUI.Windows
+{
+    /// <summary>
+    /// Represents the tool strip merging strategy.
+    /// </summary>
+    public class ToolStripMerge
+    {
+        /// <summary>
+        /// Initializes a new instance of the <c>ToolStripMerge</c> class.
+        /// </summary>
+        /// <param name="toolStrip">The <see cref="ToolStrip"/> to be merged into the main window.</param>
+        public ToolStripMerge(ToolStrip toolStrip)
+            : this(toolStrip, true)
+        { }
+
+        /// <summary>
+        /// Initializes a new instance of the <c>ToolStripMerge</c> class.
+        /// </summary>
+        /// <param name="toolStrip">The <see cref="ToolStrip"/> to be merged into the main window.</param>
+        /// <param name="autoHide">A <see cref="bool"/> value which indicates whether the merged tool strip should
+        /// be hidden when the window that contains this merged tool strip is hidden.</param>
+        public ToolStripMerge(ToolStrip toolStrip, bool autoHide)
+        {
+            ToolStrip = toolStrip;
+            AutoHide = autoHide;
+        }
+
+        /// <summary>
+        /// Gets a <see cref="bool"/> value which indicates whether the merged tool strip should
+        /// be hidden when the window that contains this merged tool strip is hidden.
+        /// </summary>
+        public bool AutoHide { get; }
+
+        /// <summary>
+        /// Gets the <see cref="ToolStrip"/> instance that is to be merged into the main window.
+        /// </summary>
+        public ToolStrip ToolStrip { get; }
+    }
+}
